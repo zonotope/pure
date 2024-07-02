8ecaf884035ae90d2644b42866492c678fcbb3c8
Benjamin Lamothe <bl@732.one>
2024-07-01 22:03:35 -0400

    don't set title from prompt

--- pure.zsh
+++ pure.zsh
@@ -53,34 +53,6 @@ prompt_pure_check_cmd_exec_time() {
         }
 }
 
-prompt_pure_set_title() {
-        setopt localoptions noshwordsplit
-
-        # Emacs terminal does not support settings the title.
-        (( ${+EMACS} || ${+INSIDE_EMACS} )) && return
-
-        case $TTY in
-                # Don't set title over serial console.
-                /dev/ttyS[0-9]*) return;;
-        esac
-
-        # Show hostname if connected via SSH.
-        local hostname=
-        if [[ -n $prompt_pure_state[username] ]]; then
-                # Expand in-place in case ignore-escape is used.
-                hostname="${(%):-(%m) }"
-        fi
-
-        local -a opts
-        case $1 in
-                expand-prompt) opts=(-P);;
-                ignore-escape) opts=(-r);;
-        esac
-
-        # Set title atomically in one print statement so that it works when XTRACE is enabled.
-        print -n $opts $'\e]0;'${hostname}${2}$'\a'
-}
-
 prompt_pure_preexec() {
         if [[ -n $prompt_pure_git_fetch_pattern ]]; then
                 # Detect when Git is performing pull/fetch, including Git aliases.
@@ -93,9 +65,6 @@ prompt_pure_preexec() {
         fi
 
         typeset -g prompt_pure_cmd_timestamp=$EPOCHSECONDS
-
-        # Shows the current directory and executed command in the title while a process is active.
-        prompt_pure_set_title 'ignore-escape' "$PWD:t: $2"
 }
 
 # Change the colors if their value are different from the current ones.
@@ -218,9 +187,6 @@ prompt_pure_precmd() {
         # Check execution time and store it in a variable.
         prompt_pure_check_cmd_exec_time
         unset prompt_pure_cmd_timestamp
-
-        # Shows the full path in the title.
-        prompt_pure_set_title 'expand-prompt' '%~'
 
         # Modify the colors if some have changed..
         prompt_pure_set_colors
